15c6f89da32935fff53883ccc2dd03dc92593477
ilnura <t.ilnura@gmail.com>
2023-08-18 13:34:15 -0600

    emoji.py added

---
+++ Day6/emoji.py
@@ -0,0 +1,20 @@
+emoji = "\U0001f600"
+
+#num = 1
+#while num <= 10:
+#    print(emoji*num)
+#    num += 1
+
+# nested loops
+#for x in range(3):
+#   for num_ in range(1, 11):
+#       print(emoji*num_)
+
+# Without string multiplication
+# for num in range(1, 11):
+#     count = 1
+#     smiles = ""
+#     while count <= num:
+#         smiles += "\U0001f600"
+#         count += 1
+#     print(smiles)
